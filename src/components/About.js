f226cb10be5dddbfb7c52666d41befdbea61a862
ZeRav3n <ze.rav3n@gmail.com>
2023-07-19 02:16:05 -0400

    Test

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ function About() {
   return (
     <div id="about" className='about'>
       <h1 data-aos="fade-up">About Me</h1>
-      <p data-aos="fade-up">I am a student at the Humber College Institute of Technology & Advanced Learning, Canada studying Computer Programming and Analysis. I am interested in website programming & development, and database management. I am currently looking for a lost semi-colon.</p>
+      <p data-aos="fade-up">I am at student at the Humber College Institute of Technology & Advanced Learning, Canada studying Computer Programming and Analysis. I am interested in website programming & development, and database management. I am currently looking for a lost semi-colon.</p>
     </div>
   );
 }
