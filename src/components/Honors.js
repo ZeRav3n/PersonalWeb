d152f6fad74f28e0d77da90d788afdaae59950ae
ZeRav3n <ze.rav3n@gmail.com>
2023-07-29 03:35:36 -0400

    Honors and Skills Update

--- src/components/Honors.js
+++ src/components/Honors.js
@@ -1,11 +1,34 @@
 import "../App.css"
-
+import Zaman_badgeF22 from "../assets/Shahid Md Ratin Zaman_badgeF22.jpg"
+import Zaman_badgeW23 from "../assets/Shahid Md Ratin Zaman_badgeW23.jpg"
 export default function Honors() {
-    return (
-      <div id="honors" className='section'>
-        <h1>Honors</h1>
-        <p>List your honors here.</p>
+  const honors = [
+    {
+    title: "Dean's List", 
+    date: "Fall 2022", 
+    description: "Awarded to students who have achieved a GPA of 3.5 or higher for the semester.",
+    imgSrc: Zaman_badgeF22,
+  },
+    {
+    title: "Dean's List",
+    date: "Winter 2023",
+    description: "Awarded to students who have achieved a GPA of 3.5 or higher for the semester.",
+    imgSrc: Zaman_badgeW23,
+  },
+  ];
+  return(
+    <div className="honors-section">
+      <h2 data-aos="fade-right">Honors & Awards</h2>
+      <div className="honors-grid">
+        {honors.map((honor, index)=>(
+          <div className="honor-card" key={index} data-aos="fade-right">
+            <h3 className="honor-title">{honor.title}</h3>
+            <p className="honor-description">{honor.description}</p>
+            <p className="honor-date">{honor.date}</p>
+            <a href={honor.imgSrc} target="_blank" rel="noopener noreferrer" className="view-btn">View Certificate</a>
+          </div>
+        ))}
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,17 +1,32 @@
-import "../App.css"
+import "../App.css";
+import React from "react";
 
 export default function Skills() {
-    return (
-      <div id="skills" className='section'>
-        <h1>Test</h1>
-        <ul>
-          UI/UX Design
-          Web Design
-          Front end
-          Back end
-          Digital Design
+  const skills = ["React", "JavaScript", "HTML", "CSS", "Node.js"];
+
+  return (
+    <div className="skills-section">
+      <div className="column">
+        <h2 data-aos="fade-up">Skills</h2>
+        <ul className="skills-list" data-aos="fade-up">
+          {skills.map((skill, index) => (
+            <li className="skill-item" key={index}>
+              {skill}
+            </li>
+          ))}
         </ul>
       </div>
-    );
-  }
-  
+      <div className="column">
+        <h2 data-aos="fade-up">Passion</h2>
+        <p data-aos="fade-up">
+          I get tremendous satisfaction and excitement when I incorporate design
+          into my daily life. Even the most commonplace things have an aesthetic
+          appeal to me, especially when they serve a useful purpose. My focus is
+          still on continuously honing and exceeding my own goals as I look to
+          the future. My enthusiasm and ambition are fueled by each success,
+          which drives me to pursue greater success.
+        </p>
+      </div>
+    </div>
+  );
+}
