f38c102f3b67d290a954976cd7618cbe7b380e1c
ZeRav3n <ze.rav3n@gmail.com>
2023-07-29 01:38:24 -0400

    Skills Update

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,6 +4,13 @@ export default function Skills() {
     return (
       <div id="skills" className='section'>
         <h1>Test</h1>
+        <ul>
+          <li>UI/UX Design</li>
+          <li>Web Design</li>
+          <li>Front end</li>
+          <li>Back end</li>
+          <li>Digital Design</li>
+        </ul>
       </div>
     );
   }
