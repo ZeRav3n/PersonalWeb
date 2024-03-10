39bfde3fbda4ce9705bbafcc02b99369a0be1723
ZeRav3n <ze.rav3n@gmail.com>
2024-03-09 23:36:16 -0500

    Some updates

--- src/components/Honors.js
+++ src/components/Honors.js
@@ -6,7 +6,8 @@ import Zaman_badgeF21 from "../assets/Shahid Md Ratin Zaman_badgeF21.pdf";
 import Zaman_badgeW22 from "../assets/Shahid Md Ratin Zaman_badgeW22.pdf";
 import Resume from "./Resume.js";
 export default function Honors() {
-  const honorsDescription = "Awarded to students who have achieved a GPA of 3.5 or higher for the semester.";
+  const honorsDescription =
+    "Awarded to students who have achieved a GPA of 3.5 or higher for the semester.";
   const deanListEntries = [
     { date: "Fall 2021", imgSrc: Zaman_badgeF21 },
     { date: "Winter 2022", imgSrc: Zaman_badgeW22 },
@@ -14,32 +15,36 @@ export default function Honors() {
     { date: "Winter 2023", imgSrc: Zaman_badgeW23 },
     { date: "Fall 2023", imgSrc: Zaman_badgeF23 },
   ];
+
   return (
     <div className="honors-section">
       <h2 data-aos="fade-right">Honors & Awards</h2>
       <div className="honors-grid">
-      <ul>
-        <li>
-          <h3 className="honor-title">Dean's List</h3>
-          <p className="honor-description">{honorsDescription}</p>
-          {deanListEntries.map((entry, index) => (
-            <div key={index}>
-              <p className="honor-date">{entry.date}</p>
-              <a
-                href={entry.imgSrc}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="view-btn"
-              >
-                View Certificate
-              </a>
+        <ul>
+          <li>
+            <h3 className="honor-title">Dean's List</h3>
+            <p className="honor-description">{honorsDescription}</p>
+            <div className="gridHonor">
+            {deanListEntries.map((entry, index) => (
+              <div className="honor-card" key={index} data-aos="fade-right">
+                <p className="honor-date">{entry.date}</p>
+                <a
+                  href={entry.imgSrc}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="view-btn"
+                >
+                  <span>View Certificate</span>
+                </a>
+                
+              </div>
+            ))}
             </div>
-          ))}
-        </li>
-      </ul>
+          </li>
+        </ul>
         <div className="resume-container" data-aos="fade-up">
-      <Resume />
-    </div>
+          <Resume />
+        </div>
       </div>
     </div>
   );
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,6 +4,7 @@ import "../App.css"
 function Intro() {
   return (
     <div id="intro" className='intro'>
+      <p style={{fontSize: 25}}>...Work in progress...</p>
       <h1 data-aos="fade-up">RATIN</h1>
       <p data-aos="fade-up">WELCOME TO... ME!</p>
     </div>
